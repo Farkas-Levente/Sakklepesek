28665cff7b0821b9b7f09a5427696406d58fe983
Farkas-Levente <89079547+Farkas-Levente@users.noreply.github.com>
2022-05-22 10:22:23 +0200

    Tabla kialakítás
    A canvasra feldob méret szerint buttonokat és sakktábla szerűen kiszínezi őket

--- Sakklepesek_FarkasLevente/MainWindow.xaml.cs
+++ Sakklepesek_FarkasLevente/MainWindow.xaml.cs
@@ -20,9 +20,67 @@ namespace Sakklepesek_FarkasLevente
     /// </summary>
     public partial class MainWindow : Window
     {
+        int meret = 10;
+        Button[,] negyzetek;
         public MainWindow()
         {
             InitializeComponent();
+            TablaKialakitasa();
+        }
+
+
+       
+        private void TablaKialakitasa()
+        {
+            tabla.Children.Clear();
+            negyzetek = new Button[meret, meret];
+            for (int i = 0; i < meret; i++)
+            {
+
+                for (int j = 0; j < meret; j++)
+                {
+
+
+                    Button negyzet = new Button();
+
+
+                    negyzet.Width = tabla.Width / meret;
+                    negyzet.Height = tabla.Height / meret;
+                    negyzet.Margin = new Thickness(tabla.Width / meret * j, tabla.Height / meret * i, 0, 0);
+
+
+                    
+
+                    tabla.Children.Add(negyzet);
+
+                    negyzetek[i, j] = negyzet;
+
+                }
+            }
+
+            for (int i = 0; i < meret; i++)
+            {
+
+                for (int j = 0; j < meret; j++)
+                {
+
+
+                    if (j % 2 == 1 && i % 2 == 1)
+                    {
+                        negyzetek[i, j].Background = Brushes.Khaki;
+                    }
+                    else if (j % 2 == 0 && i % 2 == 0)
+                    {
+                        negyzetek[i, j].Background = Brushes.Khaki;
+                    }
+                    else
+                    {
+                        negyzetek[i, j].Background = Brushes.IndianRed;
+                    }
+
+                }
+            }
+
         }
     }
 }
